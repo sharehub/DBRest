350eafab5dc9ddce93b9c2cc820e41a343f041cd
Leon <dontmailme@notexist.com>
2014-01-17 17:31:49 +0800

    daily checkin

--- dbrest/setting.py
+++ dbrest/setting.py
@@ -6,6 +6,14 @@ import string
 IDCODE='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 MULTICODE=[2, 3, 5, 7, 11, 13]
 
+def secretkey():
+    #invalid = ['"', "'"]
+    codes = [chr(c) for c in range(ord('!'), ord('~')) if (c<ord('A') or c>ord('Z'))]
+    codes.remove('"')
+    codes.remove("'")
+    idx = [random.randrange(len(codes)) for i in range(50)]
+    return [codes[i] for i in idx]
+
 def codegen():
     codelist = []
     thelist = [ch for ch in IDCODE]
@@ -26,6 +34,7 @@ if __name__ == '__main__':
     # print("PROFILE_CODE='"+string.join(codegen(), '')+"'")
     print("PROFILE_CODE='"+''.join(random.sample(IDCODE, len(IDCODE)))+"'")
     print("PROFILE_MULTI=["+','.join([str(i) for i in multigen()])+"]")
+    print("SECRET_KEY='" + ''.join(secretkey()) + "'")
 
 #else:
     # no, 
