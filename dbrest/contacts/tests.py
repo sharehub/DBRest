595c84628218c40637a5c7c2e446fc868c921fa9
Leon <dontmailme@notexist.com>
2014-01-27 10:55:54 +0800

    fixed the test case

--- dbrest/contacts/tests.py
+++ dbrest/contacts/tests.py
@@ -1,11 +1,23 @@
 #from django.utils import unittest
 from django.test import TestCase
 from django.test.client import Client
+from django.contrib.auth.models import User
 from profile import ProfileView
 
 # Create your tests here.
 
 class ContactAPITestCase(TestCase):
+    """
+    Create a user and use this user for testing
+    The test system will user a standalone DB instead of the default DB
+    """
+    def setUp(self):
+        User.objects.create_user('itest', password='itest', email='itest@itest.com')
+
+    def tearDown(self):
+        itest = User.objects.get(username='itest')
+        itest.delete()
+
     def test_logout(self):
         """ logout and redirect to '/'"""
         r = Client().get('/contacts/api/auth')
@@ -13,7 +25,7 @@ class ContactAPITestCase(TestCase):
 
     def test_login(self):
         """ login use  test password """
-        r = Client().post('/contacts/api/auth', {'username':'itest', 'password':'itest'})
+        r = Client().post('/contacts/api/auth', {'username':r'itest', 'password':r'itest'})
         print(r.content)
         self.assertEqual(r.status_code, 200)
         self.assertTrue("Success" in r.content)
